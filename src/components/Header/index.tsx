075d1b2aabb59ed7cff98d5aba6dd3aaa19f43dd
Diego Masin <diegoifce@gmail.com>
2021-12-04 17:23:13 -0300

    Build modal with react-modal.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
+import Modal from "react-modal";
+
 import logoImg from "../../assets/logo.svg";
+
 import { Container, Content } from "./style";
 
 export function Header() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="sic financeiro" />
-        <button type="button">Nova Transação</button>
+        <button type="button" onClick={handleOpenNewTransactionModal}>
+          Nova Transação
+        </button>
+        <Modal
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={handleCloseNewTransactionModal}
+        >
+          <h2>Cadastrar Transação</h2>
+        </Modal>
       </Content>
     </Container>
   );
