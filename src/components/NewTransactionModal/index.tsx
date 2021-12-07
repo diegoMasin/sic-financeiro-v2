b03b6da67f8bebf1345b64f63829af170b9ecd26
Diego Masin <diegoifce@gmail.com>
2021-12-06 21:57:07 -0300

    Working on Modal's content - part 4 - new buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,9 @@
 import Modal from "react-modal";
 
-import { Container } from "./style";
+import { Container, TransactionTypeContainer } from "./style";
 import closeImg from "../../assets/close.svg";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -30,6 +32,16 @@ export function NewTransactionModal({
         <h2>Cadastrar Transação</h2>
         <input placeholder="Título" />
         <input placeholder="Valor" type="number" />
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+          <button type="button">
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
         <input placeholder="Categoria" />
         <button type="submit">Cadastrar</button>
       </Container>
