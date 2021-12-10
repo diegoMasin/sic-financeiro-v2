a81ae5183ccfbb90acf5bb5c06c04a34498c3c71
Diego Masin <diegoifce@gmail.com>
2021-12-09 22:19:44 -0300

    Working on Modal's content - part 7 - Seeds Mirage

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createServer, Model } from "miragejs";
 import { App } from "./App";
-import { request } from "http";
 
 createServer({
   models: {
     transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Curso de React",
+          type: "withdraw",
+          category: "Dev",
+          amount: 3000,
+          createdAt: new Date("2021-09-12 20:00:00"),
+        },
+        {
+          id: 2,
+          title: "Salário",
+          type: "deposit",
+          category: "Trabalho",
+          amount: 7000,
+          createdAt: new Date("2021-08-12 08:00:00"),
+        },
+      ],
+    });
   },
 
   routes() {
