e7b4d3df0ac8af4700887c2b24202325f1135254
Diego Masin <diegoifce@gmail.com>
2021-12-09 23:17:16 -0300

    Working with Context.

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 
 import { GlobalStyle } from "./styles/global";
+import { TransactionsProvider } from "./TransactionsContext";
 
 Modal.setAppElement("#root");
 
@@ -22,14 +23,17 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
+
       <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
       />
+
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
